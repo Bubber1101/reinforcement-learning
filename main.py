139db8a7b539a9dbf84c1d8c0048acd66da3cd4f
Patryk Stradomski <patrykstradomski@wp.pl>
2021-05-08 21:26:14 +0200

    Removed worng requirements.txt and unused script for DQN, started work with pylint

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from gym_unity.envs import UnityToGymWrapper
 from mlagents_envs.environment import UnityEnvironment
 
+from basic_sarsamax import basic_q_learning
+
 plt.style.use('seaborn-whitegrid')
 
 
@@ -10,7 +12,7 @@ def random_action(path_to_executable):
     env = UnityToGymWrapper(unity_env)
     for i_episode in range(20):
         env.reset()
-        for t in range(50):
+        for t in range(20):
             env.render()
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
@@ -35,20 +37,16 @@ def single_action(path, action):
 
 
 def move_left():
-    single_action("C:/Users/patry/Dev/Thesis/envs/Basic/Basic.exe", 1)
+    single_action("envs/Basic.exe", 1)
 
 
 def move_right():
-    single_action("C:/Users/patry/Dev/Thesis/envs/Basic/Basic.exe", 2)
+    single_action("envs/Basic.exe", 2)
 
 
 if __name__ == '__main__':
-    # randomAction("C:/Users/patry/Dev/Thesis/envs/Worm/UnityEnvironment.exe")
-    # random_action("C:/Users/patry/Dev/Thesis/envs/Basic/Basic.exe")
-    move_left()
-    # move_right()
-    # scores, q_table = basic_q_learning(number_of_episodes=100, epsilon_decays=False, epsilon=0.8, learning_rate=0.9,
-    #                                    discount_factor=0.98)
-    # plt.plot(scores)
-    # plt.show()
-    # print(q_table)
+    scores, q_table = basic_q_learning(number_of_episodes=100, epsilon_decays=False, epsilon=0.8, learning_rate=0.9,
+                                       discount_factor=0.98)
+    plt.plot(scores)
+    plt.show()
+    print(q_table)
