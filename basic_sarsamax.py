5f0a3fb5a8c1f7c530246eb3f8199d9f58b7be88
Patryk Stradomski <patrykstradomski@wp.pl>
2021-05-08 21:26:14 +0200

    Removed worng requirements.txt and unused script for DQN, started work with pylint

--- basic_sarsamax.py
+++ basic_sarsamax.py
@@ -9,8 +9,15 @@ def basic_q_learning(discount_factor=0.95,
                      epsilon_decay=0.5,
                      epsilon_min=0.01,
                      number_of_episodes=100,
-                     epsilon_decays = True):
-    unity_env = UnityEnvironment("C:/Users/patry/Dev/Thesis/envs/Basic/Basic.exe", no_graphics=False)
+                     epsilon_decays=True):
+    """
+        This performs a sarsamax training on environment "Basic" located under "envs/Basic.exe".
+        The environment was prepared in unity and this script uses
+        Unity ML-Agents Python Interface(https://github.com/Unity-Technologies/ml-agents/tree/main/ml-agents-envs)
+        in order to communicate with the executable.
+    """
+
+    unity_env = UnityEnvironment("envs/Basic.exe", no_graphics=False)
     env = UnityToGymWrapper(unity_env)
     possible_states = env.observation_space.shape[0]
     possible_actions = env.action_space.n
@@ -26,7 +33,7 @@ def basic_q_learning(discount_factor=0.95,
         done = False
         while not done:
             action = choose_explore_exploit(q_table, state, epsilon, env.action_space)
-            next_state, reward, done, info = env.step(action)
+            next_state, reward, done, _ = env.step(action)
             if next_state is not None:
                 next_state = np.where(next_state == 1)[0][0]
             score = score + reward
@@ -44,7 +51,13 @@ def basic_q_learning(discount_factor=0.95,
     return scores, q_table
 
 
-def update_q(q_table, reward, learning_rate, discount_factor, current_state, current_action, next_state):
+def update_q(q_table,
+             reward,
+             learning_rate,
+             discount_factor,
+             current_state,
+             current_action,
+             next_state):
     next_q = get_next_q(next_state, q_table)
 
     current_q = q_table[current_state][current_action]
